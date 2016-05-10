64d6de4ee8e29f75eb8136117bcd4706751b9ca7
euglazer <eugenestapler@gmail.com>
2016-05-10 15:43:36 -0700

    add progress indicator

--- notes.md
+++ notes.md
@@ -1,24 +1,8 @@
-#### currently
+### todo:
 
-  - user uploads video
-  - html5 video created from file blob
-  - on video load, encoder started, and initialized with repeat, delay, and quality
-  - skip frame by frame through video, and for each frame:
-    - draw html5's current image to a canvas
-    - detect faces from that canvas
-    - for each detected face
-      - draw face to croppedCanvas
-      - add croppedCanvas frame to encoder
-  - when finished:
-    - finish encoder
-    - get encoderData
-    - create GIF blob
-    - download blob as GIF
-
-#### optimization strategies
-
-  - can we extract faces, directly from HTML5 video, instead of sending to canvas and then reading from canvas?
-  - send raw 1D rgba bitmap to gifencoder via stream
-  - use face's scalex and scaley to avoid rawCanvas vs croppedCanvas? (hmm idk)
-  - let user choose what frame rate they want
-  - experiment with GIF quality
+  - refactor
+  - progress
+  - gif filename
+  - gif quality
+  - gif size
+  - frameTime
--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 import $ from 'jquery'
 import faceDetection from 'jquery-facedetection'
 faceDetection($)
-import Frame from 'canvas-to-buffer'
 import GIFEncoder from 'gifencoder'
 import download from 'downloadjs'
 
-var croppedCanvasSize = 150;
-var $croppedCanvas = $(`<canvas id="cropped-canvas" width="${croppedCanvasSize}" height="${croppedCanvasSize}"></canvas>`)
-var croppedCanvasContext = $croppedCanvas[0].getContext('2d')
-$('body').append($croppedCanvas)
-var encoder = new GIFEncoder(croppedCanvasSize, croppedCanvasSize)
+var previewCanvasSize = 150;
+var $previewCanvas = $(`<canvas id="preview-canvas" width="${previewCanvasSize}" height="${previewCanvasSize}"></canvas>`)
+var previewCanvasContext = $previewCanvas[0].getContext('2d')
+$('body').append($previewCanvas)
+var encoder = new GIFEncoder(previewCanvasSize, previewCanvasSize)
+var $progressIndicator = $('#progress-indicator')
 
-var $videoPlayer;
-var frameTime = 1 / 3
+var $videoPlayer = $('#video-player')
+var frameTime = 1 / 25
 
 $('#video-file').on('change', function (e) {
   var videoFile = this.files[0]
-  $videoPlayer = $('#video-player')
   var videoSrc = window.URL.createObjectURL(videoFile)
   $videoPlayer.attr('src', videoSrc)
 
   var i = 0
   $videoPlayer.on('loadeddata', function () {
-    var $this = $(this)
-
-    $videoPlayer[0].currentTime = i
+    this.currentTime = i
     encoder.start()
     encoder.setRepeat(0)
-    encoder.setDelay(frameTime)
+    // encoder.setDelay(frameTime * 1000)
+    encoder.setDelay(40)
     encoder.setQuality(10)
   })
 
   $videoPlayer.on('seeked', function () {
     addFaceFramesToGIF()
     i += frameTime
+    updateProgressIndicator(i)
 
     if (i < $videoPlayer[0].duration) {
       $videoPlayer[0].currentTime = i
@@ -45,14 +44,19 @@ $('#video-file').on('change', function (e) {
     }
   })
 
-  function addFaceFramesToGIF () {
+  function updateProgressIndicator (i) {
+    var progressValue = (i / $videoPlayer[0].duration * 100).toFixed(2)
+    $progressIndicator.text(`${progressValue}%`)
+  }
 
+  function addFaceFramesToGIF () {
     $videoPlayer.faceDetection({
-      minneighbors: 4,
+      interval: 1,
+      minneighbors: 1,
       complete: function (faces) {
         faces.forEach((face) => {
-          croppedCanvasContext.drawImage($videoPlayer[0], face.x, face.y, face.width, face.height, 0, 0, croppedCanvasSize, croppedCanvasSize)
-          encoder.addFrame(croppedCanvasContext)
+          previewCanvasContext.drawImage($videoPlayer[0], face.x, face.y, face.width, face.height, 0, 0, previewCanvasSize, previewCanvasSize)
+          encoder.addFrame(previewCanvasContext)
         })
       }
     })
