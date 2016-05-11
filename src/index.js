bf74154f4d0b6091cc087c2d97169f5e5604379c
euglazer <eugenestapler@gmail.com>
2016-05-10 22:20:45 -0700

    style GIF

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ $('#upload-btn').click(() => {
 $('#video-file').on('change', function (e) {
   downloadFacesFromVideoAsGIF({
     gifSize: 200, // 100 -- 500
-    frameRate: 5, // 1 -- 25
+    frameRate: 1, // 1 -- 25
     file: this.files[0],
     onLoad: function (canvas) {
       $('img').remove()
@@ -23,7 +23,7 @@ $('#video-file').on('change', function (e) {
     onComplete: function (blob) {
       $('canvas').remove()
       var src = window.URL.createObjectURL(blob)
-      var $gif = $(`<img src="${src}"/>`)
+      var $gif = $(`<img id="gif" src="${src}"/>`)
       $('#upload-container').append($gif)
       $progressIndicator.text('100%')
       $('#upload-btn').show()
